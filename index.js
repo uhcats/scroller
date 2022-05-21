a967cd883f880e3594fc8fde2e05e70de5eb1295
Stanisław <stanislawplust@gmail.com>
2022-05-21 21:37:19 +0200

    Add some style, normalize.css and basic javascript

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
 
+
+  const rootElement = document.querySelector('#root');
+
+  const sections = document.querySelectorAll('section');
+
+  console.log(sections);
+
+  document.addEventListener('wheel', function (event) {
+    console.log(event.wheelDelta);
+
+  });
+})
