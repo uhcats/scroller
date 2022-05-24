5162f2a89ffd0a648f728e15b44d408d695d473e
Stanisław <stanislawplust@gmail.com>
2022-05-24 22:56:21 +0200

    Add scroll feauture

--- index.js
+++ index.js
@@ -5,10 +5,39 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const sections = document.querySelectorAll('section');
 
+  let currentSectionIndex = 0;
+
+  let isThrottled = false;
+
   console.log(sections);
 
   document.addEventListener('wheel', function (event) {
+
+    if (isThrottled) return;
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false;
+    }, 1000)
+
+    const direction = event.wheelDelta < 0 ? 1 : -1;
     console.log(event.wheelDelta);
 
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const firstSection = currentSectionIndex === 0;
+      if (firstSection) return;
+    }
+
+    currentSectionIndex = currentSectionIndex + direction;
+
+    console.log(currentSectionIndex);
+
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    })
   });
 })
